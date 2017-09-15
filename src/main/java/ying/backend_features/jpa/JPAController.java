5aebfb0988bef01c200b3fdec0f3daf15a41683c
Ying <sco.suen@gmail.com>
2017-09-14 21:40:53 -0400

    feat: JPA pageable

--- src/main/java/ying/backend_features/jpa/JPAController.java
+++ src/main/java/ying/backend_features/jpa/JPAController.java
@@ -4,7 +4,11 @@ import com.github.wenhao.jpa.Specifications;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,16 +33,25 @@ public class JPAController {
         return userProfileRepository.findById(id);
     }
 
+    /**
+     * https://127.0.0.1:8443/jpa?name=y&bio=sdf&page=0&size=2&sort=id,asc&sort=name,desc
+     * @param id
+     * @param name
+     * @param bio
+     * @param pageable
+     * @return
+     */
     @GetMapping
-    public List<UserProfile> getUserProfile (@RequestParam(value = "id", required = false) Long id,
+    public Page<UserProfile> getUserProfile (@RequestParam(value = "id", required = false) Long id,
                                              @RequestParam(value = "name", required = false) String name,
-                                             @RequestParam(value = "bio", required = false) String bio) {
+                                             @RequestParam(value = "bio", required = false) String bio,
+                                             @PageableDefault(size = 10, sort = { "id" }, direction = Sort.Direction.DESC) Pageable pageable) {
         Specification<UserProfile> specification = Specifications.<UserProfile>and()
                 .eq(id != null, "id", id)
                 .like(StringUtils.isNotBlank(name), "name", "%" + name + "%")
                 .like(StringUtils.isNotBlank(bio), "bio", "%" + bio + "%")
                 .build();
 
-        return userProfileRepository.findAll(specification);
+        return userProfileRepository.findAll(specification, pageable);
     }
 }
